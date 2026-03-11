c679f1d1e1646cf515b975fcfbdeb629c6e9df7a
ajayux <ajay@intersmart.in>
2026-03-11 18:58:56 +0530

    feat: create BlogDetailSection component to display detailed blog post content including title, author, date, main image, description, and image gallery.

--- src/components/blocks/blogs/blog-detail-section.jsx
+++ src/components/blocks/blogs/blog-detail-section.jsx
@@ -26,8 +26,7 @@ export default function BlogDetailSection({ data }) {
                 alt={"icon-news-right"}
                 width={32}
                 height={32}
-                className=
-                  "w-3 sm:w-4.5 xl:w-5 2xl:w-6.5 3xl:w-8 block"
+                className="w-3 sm:w-4.5 xl:w-5 2xl:w-6.5 3xl:w-8 block"
               />
               Share
             </Button>
@@ -38,18 +37,15 @@ export default function BlogDetailSection({ data }) {
               size="none"
               className="text-[12px] lg:text-[14px] 2xl:text-[18px] 3xl:text-[24px] leading-tight font-medium text-center text-[#008DD2] [&>before]:absolute"
             >
-              {(data.date)}
+              {data.date}
             </Text>
-            <div
-              className="w-0.5 xl:w-0.5 3xl:w-0.75 h-3 xl:h-4 2xl:h-5 bg-[#008DD2] mx-2 2xl:mx-5"
-            >
-            </div>
+            <div className="w-0.5 xl:w-0.5 3xl:w-0.75 h-3 xl:h-4 2xl:h-5 bg-[#008DD2] mx-2 2xl:mx-5"></div>
             <Text
               as="div"
               size="none"
               className="text-[12px] lg:text-[14px] 2xl:text-[18px] 3xl:text-[24px] leading-tight font-medium text-center text-[#008DD2]"
             >
-              {(data.author)}
+              {data.author}
             </Text>
           </div>
           <div className="w-full mb-5 xl:mb-7.5 2xl:mb-10 3xl:mb-12.5">
@@ -89,7 +85,6 @@ export default function BlogDetailSection({ data }) {
             {parse(data?.text)}
           </div>
         </div>
-        
       </div>
     </section>
   );
