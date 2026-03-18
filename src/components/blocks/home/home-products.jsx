73e5b2c277ecfb237430f39617173ab3b15f6870
ajayux <ajay@intersmart.in>
2026-03-18 10:43:50 +0530

    feat: add HomeProducts component to display categorized products with dynamic loading and category selection.

--- src/components/blocks/home/home-products.jsx
+++ src/components/blocks/home/home-products.jsx
@@ -184,7 +184,7 @@ function ProductBlock({ sectionData, parentTitle, reversed }) {
           </MediaQuery>
           <div className={cn("w-full lg:flex-1 max-sm:pr-4")}>
             <div className="w-full lg:min-h-[570px] xl:min-h-[680px] 2xl:min-h-[870px] 3xl:min-h-[1000px] bg-[linear-gradient(to_bottom,#008dd2b3_0%,#181818b3_30%,#181818b3_70%,#008dd2b3_100%)] rounded-[13px] 2xl:rounded-[16px] 3xl:rounded-[20px] px-1 min-[376px]:px-5 sm:px-10 lg:px-15 xl:px-18 2xl:px-7 3xl:px-25 py-5 sm:py-10 xl:py-12.5 2xl:py-15 3xl:py-[75px] ">
-              <div className="flex flex-wrap relative min-h-[200px]">
+              <div className="flex flex-wrap">
                 <Suspense
                   fallback={
                     <>
