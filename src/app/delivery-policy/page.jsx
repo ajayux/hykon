eb76a194f341567c014b5db2bf7dcea2148191d4
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-03-23 16:34:51 +0530

    created privacy and delivery policies

---
+++ src/app/delivery-policy/page.jsx
@@ -0,0 +1,38 @@
+import { notFound } from "next/navigation";
+import TermsAndConditions from "@/components/blocks/terms/terms-conditions";
+import { getMetaData } from "@/lib/api/metaApi";
+
+export async function generateMetadata() {
+  const { title, description, keywords, twitter, openGraph, alternates, other } =
+    await getMetaData("terms");
+  return { title, description, keywords, twitter, openGraph, alternates, other };
+}
+
+export default async function TermsPage() {
+  let termsData = null;
+
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const res = await fetch(`${baseUrl}/api/delivery-policy`, {
+      next: { revalidate: 0 },
+    });
+
+    if (res.ok) {
+      const response = await res.json();
+      termsData = response.data;
+    }
+
+    console.log(termsData)
+  } catch (error) {
+    console.error("Error fetching terms data:", error);
+  }
+  if (!termsData) {
+    notFound();
+  }
+
+  return (
+    <>
+      <TermsAndConditions data={termsData?.policy} />
+    </>
+  );
+}
