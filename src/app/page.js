d735f8b8f90c28ff55c32bdf6f26008e8d77c3d3
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-06-09 12:48:16 +0530

    fixed issue

--- src/app/page.js
+++ src/app/page.js
@@ -54,7 +54,7 @@ export default async function HomePage() {
 
  try {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const res = await fetch(`${baseUrl}/api/homez`, {
+    const res = await fetch(`${baseUrl}/api/home`, {
       next: { revalidate: 60 },
     });
 
