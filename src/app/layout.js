767d0f524f46a33d17d07f994bf65df995d45dff
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-08-06 11:26:37 +0530

    Added chat bot

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ import FloatNavigation from "@/components/common/float-navigation";
 import HomeQuestions from "@/components/blocks/home/home-questions";
 import Providers from "./providers";
 import {GoogleTagManager} from "@next/third-parties/google"
+import Script from "next/script";
 
 export const metadata = {
   verification: {
@@ -268,9 +269,12 @@ export default async function RootLayout({ children }) {
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
-        /> 
+        />
       </head> */}
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
+      <Script id="xbotic-chat-widget" strategy="afterInteractive">
+        {`!function(e,t,a){var c=e.head||e.getElementsByTagName("head")[0],n=e.createElement("script");n.async=!0,n.defer=!0,n.type="text/javascript",n.src=t+"/static/js/chat_widget.js?config="+JSON.stringify(a),c.appendChild(n)}(document,"https://xbotic.cbots.live",{bot_key:"1fb6735e3c794a9f",welcome_msg:true,branding_key:"xbotic",server:"https://xbotic.cbots.live",e:"p"});`}
+      </Script>
       <body className={cn("antialiased", fontClassName, fontVariable)}>
 
         <FloatNavigation />
